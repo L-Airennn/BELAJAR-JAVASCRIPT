42d158e0fe17c01908ef6a12b36cfcc37eda1569
L-Airennn <muhammadmumin082@gmail.com>
2025-01-20 22:15:32 +0700

    excercise

--- scripts/10-rock-paper-scissors.js
+++ scripts/10-rock-paper-scissors.js
@@ -58,8 +58,8 @@ function playGames(playerMove){
   document.querySelector('.js-result').innerHTML = result;
 
   document.querySelector('.js-moves').innerHTML = `You
-<img src="images/${playerMove}-emoji.png" class="move-icon">
-<img src="images/${computerMove}-emoji.png"class="move-icon">
+<img src="../images/${playerMove}-emoji.png" class="move-icon">
+<img src="../images/${computerMove}-emoji.png"class="move-icon">
 Computer`;
 
 }
