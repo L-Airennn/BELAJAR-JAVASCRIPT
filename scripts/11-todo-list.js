6af5459efaa6c0e4f9732b8b863aa6ca40823161
L-Airennn <muhammadmumin082@gmail.com>
2025-01-31 04:01:02 +0700

    11 todo project final code js

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,12 +1,35 @@
-const todoList=[];
+const todoList=['make dinner', 'wash dishes'];
 
-function addTodo(){
-  const inputElement= document.querySelector('.js-name-input');
-  const name = inputElement.value;
-  
-  todoList.push(name);
-  console.log(todoList);
+renderTodoList();
 
-  inputElement.value = '';
+function renderTodoList() {
+
+  let todoListHTML = '';
   
-}
+/* this code bellow is called techique Generating the HTML, instead of writing all the html by hand, we loop through an array and we generated the html using javaScript */
+
+ 
+  for(let i=0; i< todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list')
+      .innerHTML = todoListHTML;
+
+  }
+
+  function addTodo(){
+    const inputElement= document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    
+    todoList.push(name);
+    console.log(todoList);
+
+    inputElement.value = '';
+
+    renderTodoList();
+  }
