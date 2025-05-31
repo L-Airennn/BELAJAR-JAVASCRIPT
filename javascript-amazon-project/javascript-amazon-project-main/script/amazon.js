d90ab8cfba9a69af684a01c85d0b91d261082c56
L-Airennn <muhammadmumin082@gmail.com>
2025-05-31 20:14:04 +0700

    13 d

--- javascript-amazon-project/javascript-amazon-project-main/script/amazon.js
+++ javascript-amazon-project/javascript-amazon-project-main/script/amazon.js
@@ -71,6 +71,9 @@ document.querySelectorAll('.js-add-to-cart')
 
       const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
 
+      const quantity = quantitySelector.value;
+      
+
 
       if (matchingItem){
         matchingItem.quantity += 1;
@@ -88,6 +91,7 @@ document.querySelectorAll('.js-add-to-cart')
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartquantity;
+
 
       console.log(cart);
     });
