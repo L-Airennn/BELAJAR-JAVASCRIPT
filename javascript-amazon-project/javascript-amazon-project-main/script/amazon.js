410fc69ecf1e9203d6cb643ce06c15b0735ed377
L-Airennn <muhammadmumin082@gmail.com>
2025-05-31 22:32:29 +0700

    13l

--- javascript-amazon-project/javascript-amazon-project-main/script/amazon.js
+++ javascript-amazon-project/javascript-amazon-project-main/script/amazon.js
@@ -94,5 +94,9 @@ document.querySelectorAll('.js-add-to-cart')
         `.js-added-to-cart-${productId}`);
 
       addedMessage.classList.add('added-to-cart-visible');
+
+      setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000)
     });
   });
